b4362eabc407be6c38e41ca9ca40f3a018075891
teejl <thomas.lockwood@utexas.edu>
2020-09-26 21:42:24 -0500

    trying sample run for n32

--- PRJ1/trial3/sim_exe.sh
+++ PRJ1/trial3/sim_exe.sh
@@ -32,30 +32,30 @@ function simulate
   echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
 
   # run simulation for oracle case
-  echo "~~~~~  ORACLE  ~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  cd ~/sesc/apps/Splash2/raytrace
-  cp ~/Repos/HPCA/PRJ1/trial3/cmp4-noc-ora.conf ~/sesc/confs/cmp4-noc-ora.conf
-  rm ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.OrA
-  ~/sesc/sesc.opt -f OrA -c ~/sesc/confs/cmp4-noc-ora.conf -ort.out -ert.err raytrace.mipseb -p1 -m128 -a3 Input/reduced.env
-  rm ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.OrA
-  cp ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.OrA ~/Repos/HPCA/PRJ1/trial3/
-  ~/sesc/scripts/report.pl ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.OrA >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  echo "~~~~~  ORACLE ~~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "~~~~~  ORACLE  ~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # cd ~/sesc/apps/Splash2/raytrace
+  # cp ~/Repos/HPCA/PRJ1/trial3/cmp4-noc-ora.conf ~/sesc/confs/cmp4-noc-ora.conf
+  # rm ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.OrA
+  # ~/sesc/sesc.opt -f OrA -c ~/sesc/confs/cmp4-noc-ora.conf -ort.out -ert.err raytrace.mipseb -p1 -m128 -a3 Input/reduced.env
+  # rm ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.OrA
+  # cp ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.OrA ~/Repos/HPCA/PRJ1/trial3/
+  # ~/sesc/scripts/report.pl ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.OrA >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "~~~~~  ORACLE ~~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
 
   # run simulation for oracle case
-  echo "~~~~~  NOT TAKEN  ~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  cd ~/sesc/apps/Splash2/raytrace
-  cp ~/Repos/HPCA/PRJ1/trial3/cmp4-noc-nta.conf ~/sesc/confs/cmp4-noc-nta.conf
-  rm ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.NTA
-  ~/sesc/sesc.opt -f NTA -c ~/sesc/confs/cmp4-noc-nta.conf -ort.out -ert.err raytrace.mipseb -p1 -m128 -a3 Input/reduced.env
-  rm ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.NTA
-  cp ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.NTA ~/Repos/HPCA/PRJ1/trial3/
-  ~/sesc/scripts/report.pl ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.NTA >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  echo "~~~~~  NOT TAKEN ~~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
-  echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "~~~~~  NOT TAKEN  ~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # cd ~/sesc/apps/Splash2/raytrace
+  # cp ~/Repos/HPCA/PRJ1/trial3/cmp4-noc-nta.conf ~/sesc/confs/cmp4-noc-nta.conf
+  # rm ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.NTA
+  # ~/sesc/sesc.opt -f NTA -c ~/sesc/confs/cmp4-noc-nta.conf -ort.out -ert.err raytrace.mipseb -p1 -m128 -a3 Input/reduced.env
+  # rm ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.NTA
+  # cp ~/sesc/apps/Splash2/raytrace/sesc_raytrace.mipseb.NTA ~/Repos/HPCA/PRJ1/trial3/
+  # ~/sesc/scripts/report.pl ~/Repos/HPCA/PRJ1/trial3/sesc_raytrace.mipseb.NTA >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "~~~~~  NOT TAKEN ~~~~~~" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
+  # echo "" >> ~/Repos/HPCA/PRJ1/trial3/results.txt
 }
 
 
