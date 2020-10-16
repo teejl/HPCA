a02191503af6b14a1e49ed4ca5a12c67d56735d2
teejl <thomas.lockwood@utexas.edu>
2020-10-15 21:06:46 -0500

    trying again...

--- PRJ2/trial2/CacheCore.cpp
+++ PRJ2/trial2/CacheCore.cpp
@@ -312,6 +312,7 @@ template<class State, class Addr_t, bool Energy>
 typename CacheAssoc<State, Addr_t, Energy>::Line
 *CacheAssoc<State, Addr_t, Energy>::findLine2Replace(Addr_t addr, bool ignoreLocked)
 {
+    std::cout << "calling that one function... \n";
     Addr_t tag    = calcTag(addr);
     Line **theSet = &content[calcIndex4Tag(tag)];
 
