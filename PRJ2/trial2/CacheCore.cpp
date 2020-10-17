2d018b72994c4fed0d755a24cd31087b0c4825a4
teejl <thomas.lockwood@utexas.edu>
2020-10-17 14:07:18 -0500

    okay making progress and putting more print statements

--- PRJ2/trial2/CacheCore.cpp
+++ PRJ2/trial2/CacheCore.cpp
@@ -333,7 +333,7 @@ typename CacheAssoc<State, Addr_t, Energy>::Line
 
         // start at 2nd to last for NXLRU
         if (policy == NXLRU) {
-            
+
             // print data to figure out tmp
             std::cout << "\n Starting findLine2Replace: \n";
             std::cout << "line:*line, isValid, isLocked, theSet, setEnd \n";
@@ -424,7 +424,7 @@ typename CacheAssoc<State, Addr_t, Energy>::Line
             Line **prev = l - 1;
             *l = *prev;;
             l = prev;
-            if (policy == NXLRU) std::cout << "**prev:" << l - 1 << " *prev:" << *prev << " prev:" << prev << "\n";
+            if (policy == NXLRU) std::cout << "**prev:" << l - 1 << " *prev:" << *prev << " prev:" << prev << " tmp" << tmp << "\n";
             //if (policy == NXLRU) std::cout << " l:" << l " \n"; 
         }
         *theSet = tmp;
