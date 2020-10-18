c6d3d904669116845bee7040b5c320d6e8b26cd5
teejl <thomas.lockwood@utexas.edu>
2020-10-18 00:16:34 -0500

    not sure just trying out random stuff now

--- PRJ2/trial3/SMPCache.cpp
+++ PRJ2/trial3/SMPCache.cpp
@@ -488,6 +488,17 @@ void SMPCache::doRead(MemRequest *mreq)
     }
     // end of compMisses
 
+    // compMisses are the unique sets of tags that enter the cache? TJL
+    // set <int, greater <int> > cm; // added above already
+    int size = cm.size();
+    cm.insert(calcTag(addr));
+    //std::cout << "Tag: " << calcTag(addr) << "\n";
+    if (size != cm.size()) {
+        compMiss.inc();
+        std::cout << cm.size() << "wr\t";
+    }
+    // end of compMisses
+
 #if (defined TRACK_MPKI)
     DInst *dinst = mreq->getDInst();
     if(dinst) {
