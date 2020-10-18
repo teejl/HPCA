fa01c3941ad5d26f8f7e1cd057044eeeada3166c
teejl <thomas.lockwood@utexas.edu>
2020-10-17 21:29:36 -0500

    scaling back a little

--- PRJ2/trial3/SMPCache.cpp
+++ PRJ2/trial3/SMPCache.cpp
@@ -478,11 +478,11 @@ void SMPCache::doRead(MemRequest *mreq)
     // compMisses are the unique sets of tags that enter the cache?
     // empty set container 
     // set <int, greater <int> > cm; // added above already
-    int size = cm.size();
-    cm.insert(l); 
-    if (size == cm.size()) {
-        compMiss.inc();
-    }
+    //int size = cm.size();
+    //cm.insert(addr); 
+    //if (size == cm.size()) {
+        //compMiss.inc();
+    //}
     // end of compMisses
     capMiss.inc();
     confMiss.inc();
