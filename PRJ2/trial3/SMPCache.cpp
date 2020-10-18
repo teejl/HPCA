fb0a617f995be683b72b0b187ad50d79cc8e7e5a
teejl <thomas.lockwood@utexas.edu>
2020-10-18 14:15:32 -0500

    adding some more here

--- PRJ2/trial3/SMPCache.cpp
+++ PRJ2/trial3/SMPCache.cpp
@@ -464,9 +464,8 @@ void SMPCache::doRead(MemRequest *mreq)
 
     // LRU REPLACEMENT ALGORITHM
     vm.push_back(calcTag(addr));
-    std::cout << "\n";
-    std::cout << "Vector begin to end: ";
-    for (auto i = vm.begin(); i <= 4; ++i) // output vector
+    std::cout << "\n Vector begin to end: ";
+    for (auto i = vm.begin(); i != vm.end() && i <= 4; ++i) // output vector
         std::cout << *i << " "; 
     // update the vector pretend cache
     // make sure this is in doWrite and doREad
