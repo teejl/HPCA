0f93e22d8a33dc512de5f02b7265e9c967018df5
teejl <thomas.lockwood@utexas.edu>
2020-11-08 18:43:47 -0600

    adding readCohe and writeCohe

--- PRJ3/trial2/SMPCache.cpp
+++ PRJ3/trial2/SMPCache.cpp
@@ -1849,6 +1849,7 @@ SMPCache::Line *SMPCache::allocateLine(PAddr addr, CallbackBase *cb,
     PAddr rpl_addr = 0;
     I(cache->findLineDebug(addr) == 0);
     Line *l = cache->findLine2Replace(addr);
+    std::cout << *l;
 
     // lets just print out cvm TJL
     // loop through original vector and update tmpv vector
