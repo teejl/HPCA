067e3bc5026e10eba2ca00e41c1b148df6d5ebef
teejl <thomas.lockwood@utexas.edu>
2020-11-10 15:47:42 -0600

    adding readCohe and writeCohe

--- PRJ3/trial2/SMPCache.cpp
+++ PRJ3/trial2/SMPCache.cpp
@@ -529,11 +529,6 @@ void SMPCache::doRead(MemRequest *mreq)
 
     // TJL CODE HERE
     if (dummy) {
-    } else if ((find(vm.begin(), vm.end(), calcTag(addr)) == vm.end())) { // in vector and miss
-        // determine if it is an actual miss
-        // && l->isLocked()
-        capMiss.inc();
-        readReplMiss.inc();
     } else if (cvm.find(calcTag(addr))!=cvm.end()) { // not in vector and miss
         // && !(l->isLocked()
         // cache->getNumLines() 
@@ -541,6 +536,11 @@ void SMPCache::doRead(MemRequest *mreq)
         // how many elements in cache
         // vector_logic();
         readCoheMiss.inc();
+    } else if ((find(vm.begin(), vm.end(), calcTag(addr)) == vm.end())) { // in vector and miss
+        // determine if it is an actual miss
+        // && l->isLocked()
+        capMiss.inc();
+        readReplMiss.inc();
     } else {
         // determine if it is an actual miss
         confMiss.inc();
