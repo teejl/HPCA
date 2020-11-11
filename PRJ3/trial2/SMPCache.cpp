3ec869379c79775c0c2cf713fc391bd96ca30b51
teejl <thomas.lockwood@utexas.edu>
2020-11-10 18:02:30 -0600

    adding readCohe and writeCohe

--- PRJ3/trial2/SMPCache.cpp
+++ PRJ3/trial2/SMPCache.cpp
@@ -1875,23 +1875,23 @@ SMPCache::Line *SMPCache::allocateLine(PAddr addr, CallbackBase *cb,
         if(canDestroyCB)
             cb->destroy();
         l->setTag(cache->calcTag(addr));
-        
+
         // [ERASE FROM CVM] TJL
-        cvm.erase(l->getOldTag()); // remove old tag
+        //cvm.erase(l->getOldTag()); // remove old tag
         DEBUGPRINT("   [%s] allocated free line for %x at %lld \n",
                    getSymbolicName(), addr , globalClock);
 
         // [ERASE FROM CVM] TJL
-        //if (l->getOldTag()!=cvm.end()){ // is it in set
-            //std::cout <<"Printing out CVM: \n";
-            //std::cout << "CVM, l, CalcTag(addr), l->getTag(), l->getOldTag() \n";
+        if (cvm.find(l->getOldTag())!=cvm.end()){ // is it in set
+            std::cout <<"Printing out CVM: \n";
+            std::cout << "CVM, l, CalcTag(addr), l->getTag(), l->getOldTag() \n";
             //for (auto i = cvm.begin(); i != cvm.end(); ++i) {
                 //std::cout << *i << ", " << l << ", " << calcTag(addr) << ", " << l->getTag() << ", " << l->getOldTag() << "\n";
             //}
             // Erase tag from list since the line is being replaced
-            //std::cout << "Erasing: CalcTag():" << calcTag(addr) << ", l:" << l << ", calcTag(rpl_addr):" << calcTag(rpl_addr) << ", l->getTag():" << l->getTag() << ", l->getOldTag():" << l->getOldTag() <<  "\n\n";
-            //cvm.erase(l->getOldTag()); // erase from set
-        //}
+            std::cout << "Erasing: CalcTag():" << calcTag(addr) << ", l:" << l << ", calcTag(rpl_addr):" << calcTag(rpl_addr) << ", l->getTag():" << l->getTag() << ", l->getOldTag():" << l->getOldTag() <<  "\n\n";
+            cvm.erase(l->getOldTag()); // erase from set
+        }
 
         return l;
     }
